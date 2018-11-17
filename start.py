4de5f8111f5d5ba27337cc8b652553b6136e380e
Cuong Nguyen <31356058+ryzzaki@users.noreply.github.com>
2018-11-17 21:14:44 +0000

    updated

--- start.py
+++ start.py
@@ -3,13 +3,14 @@ import edit
 import upload as up
 
 class Start:
-    
+
     def __init__(self, topic=rs.query):
         self.topic = topic
     
     def return_topic(self):
         return self.topic
 
+# run start
 if __name__ == "__main__":
     edit = edit.Editor()
     st = Start(rs.query)
