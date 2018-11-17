5eb89457862cadc648b30b205bda03fb83f0eb5f
Jan <32545565+jankolarik@users.noreply.github.com>
2018-11-17 18:08:37 +0000

    Update start.py

--- start.py
+++ start.py
@@ -1,11 +1,12 @@
 import research as rs
 import edit
+import upload as up
 
 class Start:
-
+    
     def __init__(self, topic=rs.query):
         self.topic = topic
-
+    
     def return_topic(self):
         return self.topic
 
@@ -16,3 +17,4 @@ if __name__ == "__main__":
     edit.path_array()
     edit.edit_clip_img()
     edit.concatenate()
+    up.upload_me()
