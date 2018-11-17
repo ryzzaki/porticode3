1133d9fc05eb9e3026b0e63f79525a14cd972796
Cuong Nguyen <31356058+ryzzaki@users.noreply.github.com>
2018-11-17 15:15:50 +0000

    madman

--- edit.py
+++ edit.py
@@ -1,1 +1,13 @@
+import moviepy
+import start
 
+st = start.Start()
+
+class Editor:
+
+    def __init__(self, topic):
+        self.topic = st.return_topic()
+    
+    def get_folder(self):
+        path = 'Pictures/' + self.topic 
+        return path
