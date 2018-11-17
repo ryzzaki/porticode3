2cbad5ee1d931b77dc6adef3fde815d10777bae4
Cuong Nguyen <31356058+ryzzaki@users.noreply.github.com>
2018-11-17 23:05:36 +0100

    fresh sounds

--- edit.py
+++ edit.py
@@ -19,7 +19,7 @@ class Editor:
     def open_json(self):
         with open('nameray.json') as f:
             self.name_array = json.load(f)
-    
+
     def generate_rand_song(self):
         num = random.randrange(0, 3)
         return num
@@ -27,11 +27,11 @@ class Editor:
     def generate_rand_rotate(self):
         num = random.randrange(0, 180)
         return num
-    
+
     def generate_rand_file(self):
         num = random.randrange(0, 34)
         return num
-    
+
     def path_array(self):
         for name in self.name_array:
             generated_num = self.generate_rand_file()
@@ -39,15 +39,16 @@ class Editor:
                 self.used_files.append(generated_num)
                 file_path = self.default_path + "/" + name
                 self.selected_files.append(file_path)
-    
+
     def edit_clip_img(self):
         for file in self.selected_files:
             # clip = mp.ImageClip(file, duration=10).add_mask().rotate(self.generate_rand_rotate())
             clip = mp.ImageClip(file, duration=5)
             self.edited_files.append(clip)
-    
+
     def concatenate(self):
-        audio = mp.AudioFileClip("audio/" + self.audio_paths[self.generate_rand_song()] + ".mp3")
+        song = "audio/" + self.audio_paths[self.generate_rand_song()] + ".mp3"
+        print("Chosen song: " + song)
         merged = mp.concatenate_videoclips(self.edited_files)
-        merged = merged.set_audio(audio.set(merged))
-        merged.write_videofile("upload.mp4",fps=24)
+        # merged = merged.set_audio(song.set_duration(merged, False))   idk why this doesnt work, help me
+        merged.write_videofile("upload.mp4",fps=24,audio=song)
