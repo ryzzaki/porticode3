77b496d31fa7b149208dd906321c7898bb42a6ab
Cuong Nguyen <31356058+ryzzaki@users.noreply.github.com>
2018-11-17 14:38:20 +0000

    updateeeeeeeeee

--- research.py
+++ research.py
@@ -1,25 +1,57 @@
-import start
 from bs4 import BeautifulSoup
 import requests
 import re
-
-st = start.Start()
-
-def download():
-    site = 'https://google.com/search?q=' + st.topic + '&tbm=isch'
-
-    response = requests.get(site)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-    img_tags = soup.find_all('img')
-
-    urls = [img['src'] for img in img_tags]
+import sys
+import os
+import http.cookiejar
+import json
+import urllib.request, urllib.error, urllib.parse
 
 
-    for url in urls:
-        filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
-        with open(filename.group(1), 'wb') as f:
-            if 'http' not in url:
-                url = '{}{}'.format(site, url)
-            response = requests.get(url)
-            f.write(response.content)
+
+def get_soup(url,header):
+    return BeautifulSoup(urllib.request.urlopen(
+        urllib.request.Request(url,headers=header)), 'html.parser')
+query = input("Enter the query: ").lower().strip()
+url="http://www.bing.com/images/search?q=" + query + "&FORM=HDRSC2"
+
+DIR='Pictures'
+
+header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+
+soup = get_soup(url,header)
+
+ActualImages=[]# contains the link for Large original images, type of  image
+
+for a in soup.find_all("a",{"class":"iusc"}):
+    mad = json.loads(a["mad"])
+    turl = mad["turl"]
+    m = json.loads(a["m"])
+    murl = m["murl"]
+    image_name = urllib.parse.urlsplit(murl).path.split("/")[-1]
+    print(image_name)
+    ActualImages.append((image_name, turl, murl))
+
+print("there are total" , len(ActualImages),"images")
+
+if not os.path.exists(DIR):
+
+    os.mkdir(DIR)
+DIR = os.path.join(DIR, query.split()[0])
+
+if not os.path.exists(DIR):
+
+    os.mkdir(DIR)
+
+for i, (image_name, turl, murl) in enumerate(ActualImages):
+
+    try:
+        raw_img = urllib.request.urlopen(turl).read()
+        cntr = len([i for i in os.listdir(DIR) if image_name in i]) + 1
+        f = open(os.path.join(DIR, image_name), 'wb')
+        f.write(raw_img)
+        f.close()
+
+    except Exception as e:
+        print("could not load : " + image_name)
+        print(e)
